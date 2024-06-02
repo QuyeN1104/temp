215d7d552bdfa256e9e09cffbd7a3e748a752204
Tuan <unlbynnt@gmail.com>
2024-06-02 12:41:42 +0700

    aduvip

--- CourseManagementSystem/user.cpp
+++ CourseManagementSystem/user.cpp
@@ -1,29 +1,9 @@
 #include "user.h"
 User::User() {};
-void User::changePassWord() {
-    string oldPass, newPass, confirmPass;
-    bool passwordChanged = false;
-    while (!passwordChanged) {
-        cout << "Enter old password: ";
-        cin >> oldPass;
-        if (oldPass == passWord) {
-            while (true) {
-                cout << "Enter new password: ";
-                cin >> newPass;
-                cout << "Confirm new password: ";
-                cin >> confirmPass;
-                if (newPass == confirmPass) {
-                    passWord = newPass;
-                    cout << "Password changed successfully!" << endl;
-                    passwordChanged = true;
-                    break;
-                }
-                else {
-                    cout << "New password and confirmation do not match. Please try again." << endl;
-                }
-            }
-        }
-        else
-            cout << "Old password is incorrect. Please try again." << endl;
+bool User::changePassWord(string oldPass, string newPass, string confirmNewPass) {
+    if (oldPass == this->passWord && newPass == confirmNewPass) {
+        this->passWord = newPass;
+        return true;
     }
+    return false;
 }
