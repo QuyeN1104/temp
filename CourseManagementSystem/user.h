a0842cddae11485a1a8a8652ace28a409a6eb32d
Tuan <unlbynnt@gmail.com>
2024-06-02 12:39:21 +0700

    add files

--- CourseManagementSystem/user.h
+++ CourseManagementSystem/user.h
@@ -2,25 +2,26 @@
 #define _USER_H_
 #include "student.h"
 struct Date {
-	int day, month, year;
+    int day, month, year;
 };
 class User {
 private:
-	string name, userName, passWord;
-	Date dmy;
-	bool sex;
-	bool isStaff;
+    string firstName, lastName , userName, passWord;
+    Date dayofBirth;
+    string gender;
+    bool isStaff;
+    string socialID;
 public:
-	User();
-	void changePassWord();
+    User();
+    bool changePassWord(string oldPass, string newPass, string confirmNewPass);
 };
 struct NodeUser {
-	User data;
-	NodeUser* next;
-	//NodeUser(User user);
+    User data;
+    NodeUser* next;
+    //NodeUser(User user);
 };
 struct LinkedList_User {
-	NodeUser* head, * tail;
-	//LinkedList_User();
+    NodeUser* head, * tail;
+    //LinkedList_User();
 };
 #endif // !_USER_H_
